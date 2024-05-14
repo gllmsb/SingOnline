08f55589b6a402d4f95bbd323a9c6491a8e54071
gllmsb <142408342+gllmsb@users.noreply.github.com>
2024-05-14 12:18:47 +0200

    hent liste

--- index.js
+++ index.js
@@ -11,20 +11,56 @@ app.get('/', (req, res) => {
     res.send('Forside')
 })
 
+app.post('/', (req, res)=>{
+    res.send('Endpoint til POST')
+})
+
 app.get('/contact', (req, res) => {
     res.send('Kontakt side')
 })
 
-app.get('/test', (req, res) => {
-    const { data, error } = supabase
+//*songs
+
+app.get('/songs', async (req, res) => {
+    const { data, error } = await supabase
         .from('songs')
-        .select('*')
+        .select('title')
     if(error) {
         console.error(error)
     }else {
         console.log(data);
+        res.send(data)
     }
 })
+
+//*artists
+
+app.get('/artists', async (req, res) => {
+    const { data, error } = await supabase
+        .from('artists')
+        .select('name')
+    if(error) {
+        console.error(error)
+    }else {
+        console.log(data);
+        res.send(data)
+    }
+})
+
+//* albums
+
+app.get('/albums', async (req, res) => {
+    const { data, error } = await supabase
+        .from('albums')
+        .select('title, artist_id, image')
+    if(error) {
+        console.error(error)
+    }else {
+        console.log(data);
+        res.send(data)
+    }
+})
+
 
 app.listen(port, () => {
     console.log('Webserver is running now on http://localhost:3000');
