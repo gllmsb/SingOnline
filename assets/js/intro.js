7d3d2e99912ffc9724b2ffeeeaeb1f927a7b21aa
gllmsb <142408342+gllmsb@users.noreply.github.com>
2024-06-04 14:44:28 +0200

    Intro, SignUp scss and index files as well as images, partials, placed pages, components and helpers, placed variables and mixins differently

---
+++ assets/js/intro.js
@@ -0,0 +1,12 @@
+(function() {
+    const hero = document.querySelector(".hero");
+    // const heroImage = document.querySelector(".hero__portal--fg img");
+
+    // heroImage.addEventListener("load", function() {
+    //    hero.classList.add('hero--animated');
+    // });
+	
+    window.addEventListener("load", function() {
+       hero.classList.add('hero--animated');
+    });
+})();
